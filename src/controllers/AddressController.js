45d1fa1282648118e6262bb59f21894f5b0c8523
marcelo-s-correa <marcelo.s.correa10@gmail.com>
2021-03-03 14:47:36 -0300

    adicao de enderecos

---
+++ src/controllers/AddressController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+const Adress = require('../models/Adress');
+
+module.exports = {
+    async store(req, res) {
+        const { user_id } = req.params;
+        const { zipcode, street, number } = req.body;
+
+        const user = await User.findByPk(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User not found' })
+        }
+
+        const adress = await Adress.create({
+            zipcode,
+            street,
+            number,
+            user_id,
+        });
+
+        return res.json(adress);
+    }
+
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const AdressController = require('./controllers/UserController');
+
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.send('Hello World!'))
+routes.get('/users', UserController.index)
 
 routes.post('/users', UserController.store);
+
+routes.post('/users/:user_id/adresses', AdressController.store);
 
 module.exports = routes;
